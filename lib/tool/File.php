218771f5a5255910dc426ed50677765b9f0b9bc0
郭玉朝 <2788828128@qq.com>
2019-04-10 00:09:18 +0800

    增加获取文件所有内容方法

--- lib/tool/File.php
+++ lib/tool/File.php
@@ -44,12 +44,27 @@ class File
         fclose($file);
     }
 
-
     private function mkFolder($path)
     {
         if (!is_readable($path)) {
             is_file($path) or mkdir($path, 0700);
         }
+    }
+
+    /**
+     * User: yuzhao
+     * CreateTime: 2019/4/10 上午12:08
+     * @param $fileName
+     * @return array
+     * Description: 获取文件所有内容
+     */
+    public function getAllFileContent($fileName) {
+        $file = fopen($this->config['file_path'] . $fileName, 'a+');
+        $returnData = array();
+        while (!feof($file)) {
+            $returnData[] = trim(fgets($file));
+        }
+        return $returnData;
     }
 
 }
